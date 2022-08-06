41eabbf5a5d89c9923b48a90faaeba27dc7fcc6d
Diamond Tusks <90947391+DiamondTusks@users.noreply.github.com>
2022-08-07 02:49:46 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
-from turtle import Screen
-from snake import Snake
-from food import Food
+import time
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
 from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
-screen.tracer()
-
-snake = Snake()
-food = Food()
+screen.title("PONG")
+screen.setup(width=800, height=600)
+screen.tracer(0)
 scoreboard = Scoreboard()
 
 
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+ball = Ball()
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
-    time.sleep(0.1)
-    snake.move()
+    ball.move()
 
-
-
-    # Detect collision with food
-    if snake.head.distance(food) < 15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
 
     # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
-    # Detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) <50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.game_start()
+        scoreboard.l_point()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.game_start()
+        scoreboard.r_point()
+
+
 
 
 screen.exitonclick()
